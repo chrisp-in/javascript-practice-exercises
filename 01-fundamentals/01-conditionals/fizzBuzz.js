2cb37c88306100ac506e3a613f65bcfac504fffb
Leon Chrispin <ufitumukizaleonchripsin@gmail.com>
2026-06-16 18:59:27 +0200

    sort and fizz buzz program

---
+++ 01-fundamentals/01-conditionals/fizzBuzz.js
@@ -0,0 +1,17 @@
+// fizzbuzz exercise
+let i = 1;
+while (i <= 100){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz");
+    }
+    else if (i % 3 === 0){
+        console.log("fizz");
+    }
+    else if (i % 5 === 0){
+        console.log("buzz");
+    }
+    else {
+        console.log(i);
+    }
+    i++;
+}
