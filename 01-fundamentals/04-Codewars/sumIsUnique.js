9074920ae8e8be8cf1c6e4ebcb54ae861e856b14
Leon Chrispin <ufitumukizaleonchripsin@gmail.com>
2026-07-28 07:41:19 +0200

    created a new file named 'sumItContinuously.js'

--- 01-fundamentals/04-Codewars/sumIsUnique.js
+++ 01-fundamentals/04-Codewars/sumIsUnique.js
@@ -1,8 +1,12 @@
 // Write a function sumUnique that takes an array of numbers as an argument and returns 
 // the sum of the elements that appear only once in the array.
-const sumIsUnique = (values) => {
-    let array = values.split("");
-  console.log(array);
-
+const sumIsUnique = values => {
+  let sortedArray = values.sort();
+  let sum = 0;
+  let filterArray = values.filter(element => {
+    if(elem)
+  })
 }
-sumIsUnique(1234);
+
+
+  
