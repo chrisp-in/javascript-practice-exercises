7caade9c39883ea2e9b3eb076e9ae43c39494457
Leon Chrispin <ufitumukizaleonchripsin@gmail.com>
2026-08-12 08:37:44 +0200

    Added some texts in the 'splitByMask.js' file

---
+++ 01-fundamentals/04-Codewars/splitByMask.js
@@ -0,0 +1,27 @@
+function split(string,mask) {
+    const arrayOfString = string.split("");
+    let sum = 0;
+    for( value of mask){
+        sum += value;
+    }
+    console.log(string.length);
+    if(string === "" && mask === []){
+        return []; 
+    }
+    else if(string.length !== sum){
+        return null;
+    }
+    else{
+        let holder;
+        const container = mask.map((element, index, array) => {
+        if(index === 0){
+            holder = (arrayOfString.splice(0,element));
+            return holder.join("");
+        }
+        holder = (arrayOfString.splice(0,element));
+        return holder.join("");
+        })
+        return container;
+    }
+}
+console.log(split("codewars",[4, 4]));
