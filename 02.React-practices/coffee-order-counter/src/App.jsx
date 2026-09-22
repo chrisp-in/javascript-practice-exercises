08731626bfd929f75d1b5430309876d55f1e5298
Leon Chrispin <ufitumukizaleonchripsin@gmail.com>
2026-09-22 19:36:28 +0200

    Updated the coffee counter by aborting the usage of props

--- 02.React-practices/coffee-order-counter/src/App.jsx
+++ 02.React-practices/coffee-order-counter/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
+
 function App() {
+  const[quantity, setQuantity] = useState(0);
+  function handleClick(){
+    setQuantity(quantity + 1);
+  }
+  function CoffeeCard(props){
+    return(
+      <div>
+         <p>{props.name}</p>
+         <p>${props.price}</p>
+         <p>{quantity}</p>
+         <button onClick={props.onClick}>Add to order</button>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Coffee Order Counter</h1>
-      <p>Welcome to our coffee shop!</p>
+      <CoffeeCard onClick={handleClick} name="Latte" price={3} />
     </div>
-  )
+  );
 }
 
 export default App
