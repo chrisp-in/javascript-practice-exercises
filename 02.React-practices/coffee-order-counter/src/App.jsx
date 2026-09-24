f53695ca247f22b3bcb4eca628802d09bc859571
Leon Chrispin <ufitumukizaleonchripsin@gmail.com>
2026-09-24 19:41:38 +0200

    Updated my coffee counter

--- 02.React-practices/coffee-order-counter/src/App.jsx
+++ 02.React-practices/coffee-order-counter/src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const[quantity, setQuantity] = useState(0);
-  function handleClick(){
-    setQuantity(quantity + 1);
-  }
-  function CoffeeCard(props){
-    return(
+  function CoffeeCard(props) {
+    const [quantity, setQuantity] = useState(0);
+
+    function handleClick() {
+      setQuantity(quantity + 1);
+    }
+
+    function decrease() {
+      if (quantity > 0) {
+        setQuantity(quantity - 1);
+      }
+    }
+
+    return (
       <div>
-         <p>{props.name}</p>
-         <p>${props.price}</p>
-         <p>{quantity}</p>
-         <button onClick={props.onClick}>Add to order</button>
+        <p>{props.name}</p>
+        <p>Price: ${props.price}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Total: ${props.price * quantity}</p>
+
+        <button onClick={handleClick}>Add to order</button>
+        <button onClick={decrease}>Remove from order</button>
       </div>
     );
   }
+
   return (
     <div>
       <h1>Coffee Order Counter</h1>
-      <CoffeeCard onClick={handleClick} name="Latte" price={3} />
+
+      <CoffeeCard name="Latte" price={3} />
+      <CoffeeCard name="Mocha" price={5} />
+      <CoffeeCard name="Espresso" price={2} />
     </div>
   );
 }
 
-export default App
+export default App;
